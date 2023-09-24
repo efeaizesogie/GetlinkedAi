45a3b870f9119906bff4d4a6dc74f12225aa3d76
efeaizesogie <efeaizesogie@gmail.com>
2023-09-24 01:24:43 +0100

    edited mobile nav

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -6,13 +6,25 @@ import { Component, ElementRef, Renderer2 } from '@angular/core';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent {
+
+  showMobileMenu: boolean = false;
+
   constructor(private renderer: Renderer2, private el: ElementRef) { }
 
   scrollToSection(sectionId: string) {
     const section = this.el.nativeElement.querySelector(`#${sectionId}`);
     if (section) {
       section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      this.showMobileMenu = false;
     }
+  }
+
+  openMobileMenu() {
+    this.showMobileMenu = true;
+  }
+
+  closeMobileMenu() {
+    this.showMobileMenu = false;
   }
 
 }
